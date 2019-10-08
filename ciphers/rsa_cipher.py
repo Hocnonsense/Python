a2610829c27872cf9288e336db5200a78bad3d9e
Hocnonsense <48747984+Hocnonsense@users.noreply.github.com>
2019-10-08 15:12:05 +0800

    Update rsa_cipher.py
    sys.exit ->raise ValueError

--- ciphers/rsa_cipher.py
+++ ciphers/rsa_cipher.py
@@ -1,4 +1,5 @@
-import sys, rsa_key_generator as rkg, os
+import os
+import rsa_key_generator as rkg
 
 DEFAULT_BLOCK_SIZE = 128
 BYTE_SIZE = 256
@@ -91,7 +92,7 @@ def encryptAndWriteToFile(
 ):
     keySize, n, e = readKeyFile(keyFilename)
     if keySize < blockSize * 8:
-        sys.exit(
+        raise ValueError(
             "ERROR: Block size is %s bits and key size is %s bits. The RSA cipher requires the block size to be equal to or greater than the key size. Either decrease the block size or use different keys."
             % (blockSize * 8, keySize)
         )
@@ -116,7 +117,7 @@ def readFromFileAndDecrypt(messageFilename, keyFilename):
     blockSize = int(blockSize)
 
     if keySize < blockSize * 8:
-        sys.exit(
+        raise ValueError(
             "ERROR: Block size is %s bits and key size is %s bits. The RSA cipher requires the block size to be equal to or greater than the key size. Did you specify the correct key file and encrypted file?"
             % (blockSize * 8, keySize)
         )
